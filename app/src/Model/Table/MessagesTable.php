f3e337afbaf76db654dbca72c2b2e1e92fc9435f
Joe <32490063+NotTheRealJoe@users.noreply.github.com>
2018-03-11 22:56:20 -0500

    Fixed bracket formatting

--- app/src/Model/Table/MessagesTable.php
+++ app/src/Model/Table/MessagesTable.php
@@ -4,10 +4,8 @@ namespace App\Model\Table;
 
 use Cake\ORM\Table;
 
-class MessagesTable extends Table
-{
-    public function initialize(array $config)
-    {
+class MessagesTable extends Table {
+    public function initialize(array $config) {
         $this->addBehavior('Timestamp');
     }
 }
