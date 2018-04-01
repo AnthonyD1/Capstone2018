9f0c0b12ad312ccc2da5fc6bba1208391f73565d
Anthony D <anthonydelgado@hotmail.com>
2018-04-01 17:42:09 -0500

    Added entity php file for mass assignments in the Cards table.

---
+++ app/src/Model/Entity/Card.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anthony
+ * Date: 4/1/18
+ * Time: 5:37 PM
+ */
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+
+class Card extends entity
+{
+    protected $_accessible = [
+        '*' => false,
+    ];
+}
