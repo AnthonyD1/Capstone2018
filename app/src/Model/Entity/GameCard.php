726ab07ff11e885798e30dab6d61aa2419b96c1f
Joe Tortorello <32490063+NotTheRealJoe@users.noreply.github.com>
2018-04-23 20:42:27 -0500

    Removed stub from trying to do associations

--- app/src/Model/Entity/GameCard.php
+++ app/src/Model/Entity/GameCard.php
@@ -6,13 +6,6 @@ use Cake\ORM\Entity;
 
 
 class GameCard extends entity {
-    /*
-    public function initialize($config) {
-        $this->belongsTo('Games')->setDependent(true)->setBindingKey('game_id')->setForeignKey('id');
-    }
-    */
-
-
     protected $_accessible = [
         '*' => true,
         'id' => false
